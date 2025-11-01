f4172201941a0ad215049d29000c6418401bc206
Lr1808 <rodulfoluis1808@gmail.com>
2025-11-01 12:13:32 -0700

    create completo

--- blogs/models.py
+++ blogs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Publication(models.Model):
@@ -13,4 +14,6 @@ class Publication(models.Model):
     
     def __str__(self):
         return self.title
-
+    
+    def get_absolute_url(self):
+        return reverse('Publication-detail', kwargs={'pk': self.pk})
