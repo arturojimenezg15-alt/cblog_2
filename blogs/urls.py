b5b049cbe859794bff16f09ab28d7742677f977b
arturojimenezg15 <arturojimenezg15@gmail.com>
2025-11-08 11:40:36 -0400

    static listo

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,12 +1,12 @@
-from django.urls import path
+from django.urls import path 
 from .views import (
                     PublicationListView, 
                     PublicationCreteView,
-                    PublicationDetailView
+                    PublicationDetailView,
                   )                   
 
 urlpatterns = [
-    path('', PublicationListView.as_view(), name='Publications-list'),
-    path('publication/new/', PublicationCreteView.as_view(), name='Publication-create'),
-    path('publication/<int:pk>/', PublicationDetailView.as_view(), name='Publication-detail'),
+    path('', PublicationListView.as_view(), name='publications-list'),
+    path('publication/new/', PublicationCreteView.as_view(), name='publication-create'),
+    path('publication/<int:pk>/', PublicationDetailView.as_view(), name='publication-detail'),
 ]
